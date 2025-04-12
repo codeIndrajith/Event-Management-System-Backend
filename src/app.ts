86ed4ccfecf3656e9c212249a1b46cb083fdccbd
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-04-12 19:16:53 +0530

    Remove clean archcitecture and use MVC architecure

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Application, NextFunction, Request, Response } from "express";
-import { connectDB } from "./infrastructure/config/db";
+import dotenv from "dotenv";
+import { connectDB } from "./config/database";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 const app: Application = express();
 
 // Database connection
 connectDB();
 
-app.listen(5000, () => console.log("Server running"));
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
--- src/core/interfaces/base-repository.ts
+++
@@ -1,7 +0,0 @@
-export interface IBaseRepository<T> {
-  create(data: Partial<T>): Promise<T | null>;
-  findById(id: string | number): Promise<T | null>;
-  findAll(): Promise<T[]>;
-  update(id: string | number, data: Partial<T>): Promise<T>;
-  delete(id: string | number): Promise<void>;
-}
